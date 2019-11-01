7200b994025a5e15ffc8e0368c3dee04301fa00b
RIDOS <41021025+RIDOS@users.noreply.github.com>
2019-11-01 23:05:23 +0500

    Laravel

---
+++ app/Http/Controllers/pagecontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class pagecontroller extends Controller
+{
+    public function page()
+    {
+        return "Инфаркт леопарда!";
+    }
+}

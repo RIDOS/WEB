f78df65ad44a3ac526a3cf733040cfa193fb9b87
RIDOS <guzairov99@bk.ru>
2020-03-06 21:39:56 +0500

    learn JS. Practic 1

---
+++ js_lern/1/js.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// let dara = confirm( 'Хелоу блять' );
+// function showMessage(dara) {
+//     (dara === true) ? alert("Ну ты ебать"): alert('Хуево');
+// }
+// showMessage(dara);
+
+
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+}
+
+function showOk() {
+    alert( "Вы согласны." );
+}
+
+function showCancel() {
+    alert( "Вы отменили выполнение." );
+}
+
+// использование: функции showOk, showCancel передаются в качестве аргументов ask
+ask("Вы согласны?", showOk, showCancel);
